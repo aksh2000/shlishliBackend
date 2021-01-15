4df0d8abd675aeb255b978226266b53acb7e909c
akshithvarma <akshithchittiveli@gmail.com>
2021-01-15 18:28:35 +0530

    userActivity -service -testing

---
+++ userActivity/src/main/java/com/shlishli/userActivity/utilities/IConstants.java
@@ -0,0 +1,8 @@
+package com.shlishli.userActivity.utilities;
+
+public interface IConstants {
+    String USER_ACTIVITY = "/userActivity";
+    String ADD_USER_ACTIVITY = "/add";
+    String GET_USER_ACTIVITIES = "/getUserActivity/{userId}";
+    String USER_ACTIVITY_TABLE = "userActivity";
+}
