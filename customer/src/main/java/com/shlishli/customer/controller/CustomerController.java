186e8cddac60410aa41b228b866771041416b1c3
akshithvarma <akshithchittiveli@gmail.com>
2021-01-15 21:41:22 +0530

    -refactor -minor

--- customer/src/main/java/com/shlishli/customer/controller/CustomerController.java
+++ customer/src/main/java/com/shlishli/customer/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.shlishli.customer.controller;
 
-import javax.ws.rs.POST;
 
 import com.shlishli.customer.entity.Customer;
 import com.shlishli.customer.service.CustomerService;
@@ -13,9 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import ch.qos.logback.classic.Logger;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(IConstants.CUSTOMERS)
--- merchant/src/main/java/com/shlishli/merchant/entity/Merchant.java
+++ merchant/src/main/java/com/shlishli/merchant/entity/Merchant.java
@@ -1,6 +1,5 @@
 package com.shlishli.merchant.entity;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
