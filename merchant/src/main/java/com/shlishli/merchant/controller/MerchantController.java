80203b88c91872031768d4379c5cc94e661e6712
akshithvarma <akshithchittiveli@gmail.com>
2021-01-15 17:27:14 +0530

    merchant -microservice -testing

---
+++ merchant/src/main/java/com/shlishli/merchant/controller/MerchantController.java
@@ -0,0 +1,32 @@
+package com.shlishli.merchant.controller;
+
+import com.shlishli.merchant.entity.Merchant;
+import com.shlishli.merchant.service.MerchantService;
+import com.shlishli.merchant.utilities.IConstants;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(IConstants.MERCHANTS)
+public class MerchantController {
+    
+
+    @Autowired
+    MerchantService merchantService;
+
+    @PostMapping(IConstants.ADD_MERCHANT)
+    public Merchant registerMerchant(@RequestBody Merchant merchant){
+        return merchantService.registerMerchant(merchant);
+    }
+
+    @GetMapping(IConstants.GET_MERCHANT_DETAILS)
+    public Merchant findCustomerById(@PathVariable("merchantId") Long merchantId){
+        return merchantService.findMerchantById(merchantId);    
+    }
+}
