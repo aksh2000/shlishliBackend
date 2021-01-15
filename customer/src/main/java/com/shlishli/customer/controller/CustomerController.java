68abf4c5b12ad6998846fb86abc5f77ea897b6cf
akshithvarma <akshithchittiveli@gmail.com>
2021-01-15 15:52:24 +0530

    customerService -added -testing

---
+++ customer/src/main/java/com/shlishli/customer/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package com.shlishli.customer.controller;
+
+import javax.ws.rs.POST;
+
+import com.shlishli.customer.entity.Customer;
+import com.shlishli.customer.service.CustomerService;
+import com.shlishli.customer.utilities.IConstants;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping(IConstants.CUSTOMERS)
+public class CustomerController {
+    
+    @Autowired
+    CustomerService customerService;
+
+
+    @PostMapping(IConstants.ADD_CUSTOMER)
+    public Customer registerCustomer(@RequestBody Customer customer){
+        return customerService.registerCustomer(customer);
+    }
+
+
+    @GetMapping(IConstants.GET_CUSTOMER_DETAILS)
+    public Customer findCustomerById(@PathVariable("customerId") String customerId){
+        return customerService.findCustomerById(customerId);    
+    }
+
+    
+}
