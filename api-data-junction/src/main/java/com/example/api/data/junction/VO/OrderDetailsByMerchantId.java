1bc120ad7e4e4fdf57a518b1ad88a83d4624d9b9
akshayk17 <akshay.k17@iiits.in>
2021-01-19 21:57:21 +0530

    -stable

---
+++ api-data-junction/src/main/java/com/example/api/data/junction/VO/OrderDetailsByMerchantId.java
@@ -0,0 +1,14 @@
+package com.example.api.data.junction.VO;
+
+import com.example.api.data.junction.entity.*;
+
+public class OrderDetailsByMerchantId {
+    public Order order;
+    public Product product;
+    public Merchant merchant;
+    public Customer customer;
+    public Inventory inventory;
+
+
+
+}
